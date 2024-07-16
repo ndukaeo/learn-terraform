a3d97d3bea41520f0a40841081d8723d7a7e3d8f
nduka ojeh <nojeh@baneollc.com>
2024-07-15 22:10:07 -0400

    starting front end setup

---
+++ roboshop-v1/ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-058ad30c15d8e84f4"]
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "mongo" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-058ad30c15d8e84f4"]
+  tags = {
+    Name = "mongo"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-041e2ea9402c46c32"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-058ad30c15d8e84f4"]
+  tags = {
+    Name = "catalogue"
+  }
+}
