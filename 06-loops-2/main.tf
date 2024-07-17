356487066fec75317f2494d66c342f101cabcfec
nduka ojeh <nojeh@baneollc.com>
2024-07-16 23:21:37 -0400

    starting front end setup

---
+++ 06-loops-2/main.tf
@@ -0,0 +1,28 @@
+resource "null_resource" "fruits" {
+  for_each = var.fruits
+}
+
+variable "fruits" {
+  default = {
+    apple = {
+      name = "apple"
+      quantity = 100
+    }
+    banana = {
+      name = "banana"
+      quantity = 20
+    }
+  }
+}
+
+resource "null_resource" "instances" {
+  for_each = var.instances
+}
+
+variable "instances" {
+  default = {
+    frontend = {}
+    catalogue = {}
+    mongo = {}
+  }
+}
