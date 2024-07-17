7feb158295a1d8a280a71a6e2ef202f5e02ea7ff
nduka ojeh <nojeh@baneollc.com>
2024-07-17 00:16:08 -0400

    starting front end setup

---
+++ 07-conditions/main.tf
@@ -0,0 +1,12 @@
+# condition in terraform is to pick a value, Not about running the block
+# = expression ? TRUE_VAL : FALSE_VAL
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z00376861T6KFA01SJSIS"
+  name    = "test.banecio-devops.online"
+  type    = "A"
+  ttl     = var.ttl == "" ? 15 : var.ttl
+  records = ["1.1.1.1"]
+}
+
+variable "ttl" {}
