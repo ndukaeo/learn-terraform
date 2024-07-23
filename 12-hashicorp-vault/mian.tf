c9601ae097d2c8008651b01c50e73f83f20402cc
nduka ojeh <nojeh@baneollc.com>
2024-07-23 11:05:05 -0400

    learning to retrieve vault

---
+++ 12-hashicorp-vault/mian.tf
@@ -0,0 +1,22 @@
+variable "vault_token" {}
+
+
+data "vault_generic_secret" "rundeck_auth" {
+  path = "kv/test"
+}
+
+provider "vault" {
+  address = "https://vault-internal.banecio-devops.online:8200"
+  token = var.vault_token
+  skip_tls_verify = true
+}
+
+resource "local_file" "foo" {
+  content  = data.vault_generic_secret.rundeck_auth.data["MYPASS"]
+  filename = "/tmp/foo.bar"
+}
+
+output "test" {
+  value = data.vault_generic_secret.rundeck_auth.data["MYPASS"]
+}
+
